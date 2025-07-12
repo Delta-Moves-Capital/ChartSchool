153362bd9e89d16877c8b71a7fbd42bc84bcb95e
JK McCain <kyle.mccain@deltamovestrading.com>
2025-07-12 10:51:04 -0500

    Update package.json for deploy

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ChartSchool/',  // ← Set this to the name of your repo
   plugins: [react()],
-})
+});
